f0812562e6450f0430e87cfe12d7bdffbd555627
hichemdz10 <157766432+hichemdz10@users.noreply.github.com>
2026-03-26 12:55:07 +0100

    Initialize state variables and save functions

--- js/state.js
+++ js/state.js
@@ -1,1 +1,47 @@
+var S = {
+    stock:    lsG("hch_stock5") || [],
+    sales:    lsG("hch_sales")  || [],
+    clients:  lsG("hch_clients")|| [],
+    expenses: lsG("hch_expenses")|| [],
+    pjobs:    lsG("hch_print")  || [],
+    flixy:    lsG("hch_flixy")  || [],
+    cart: [],
+    disc: 0,
+    tab:  lsG("hch_tab") || "home",
+    srch: "",
+    ssrch: "",
+    sortBy: "def",
+    rep: "today",
+    hideNums: false,
+    stockView: "shop",
+    openCats: {},
+    cameraActiveInHome: false,
+    form:  { n:"", p:"", c:"", q:"", wq:"", dest:"shop", cat:"كراريس", bc:"" },
+    cForm: { name:"", phone:"", debt:"" },
+    eForm: { lbl:"", amt:"", cat:"إيجار" },
+    pForm: { type:"طباعة وثيقة", pages:"", price:"" }
+};
 
+var toastTimer   = null;
+var clockTimer   = null;
+var midnightTimer= null;
+var pieChart     = null;
+
+function save() {
+    lsS("hch_stock5",   S.stock);
+    lsS("hch_sales",    S.sales);
+    lsS("hch_clients",  S.clients);
+    lsS("hch_expenses", S.expenses);
+    lsS("hch_print",    S.pjobs);
+    lsS("hch_flixy",    S.flixy);
+    lsS("hch_tab",      S.tab);
+    pushToCloud();
+}
+
+function scheduleMidnight() {
+    if (midnightTimer) clearTimeout(midnightTimer);
+    var now = new Date();
+    var midnight = new Date(now);
+    midnight.setHours(24, 0, 0, 0);
+    midnightTimer = setTimeout(function() { render(); scheduleMidnight(); }, midnight - now);
+}
